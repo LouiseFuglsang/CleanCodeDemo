113188281b7cc30d2e08e4597f24db1de4b3c169
Louise Fuglsang Mortensen <70880930+LouiseFuglsang@users.noreply.github.com>
2021-03-06 19:35:44 +0100

    Reformattet code

--- app/src/main/java/com/example/cleancodedemo/MainActivity.java
+++ app/src/main/java/com/example/cleancodedemo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.cleancodedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,43 +20,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editInput  = (EditText) findViewById(R.id.editText);
+        editInput = (EditText) findViewById(R.id.editText);
         result = (TextView) findViewById(R.id.textView);
         button = (Button) findViewById(R.id.button);
 
- /*       final TextView textView = (TextView) findViewById(R.id.textView); //get the id for TextView
-        final EditText editText = (EditText) findViewById(R.id.editText);//get the id for edit text
-        Button changeText = (Button) findViewById(R.id.button); //get the id for button
-*/
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (editInput.getText().toString() != null)//check whether the entered text is not null
+                if (editInput.getText().toString() != null)
                 {
-                    Toast.makeText(getApplicationContext(), editInput.getText().toString(), Toast.LENGTH_LONG).show();//display the text that you entered in edit text
+                    Toast.makeText(getApplicationContext(), editInput.getText().toString(), Toast.LENGTH_LONG).show();
                 }
-
                 String input = editInput.getText().toString();
-                result.setText( input );
- //               textView.setText(); //set the text after clicking button
+                result.setText(input);
             }
         });
 
-/*        final EditText editText = (EditText) findViewById(R.id.editText);//get the id for edit text
-        Button button = (Button) findViewById(R.id.button);//get the id for button
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (editText.getText().toString() != null)//check whether the entered text is not null
-                {
-                    Toast.makeText(getApplicationContext(), editText.getText().toString(), Toast.LENGTH_LONG).show();//display the text that you entered in edit text
-                }
     }
-            }
-        );
-  */  }
 }
 
 
